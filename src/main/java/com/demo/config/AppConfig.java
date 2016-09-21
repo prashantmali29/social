d4274f752bd2c9be8334b93c1d8301e9e41feb6e
prashantmali29 <prashantganpatimali@gmail.com>
2016-09-21 18:31:22 +0530

    Appconfig

--- src/main/java/com/demo/config/AppConfig.java
+++ src/main/java/com/demo/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.demo.config;
 
+
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
